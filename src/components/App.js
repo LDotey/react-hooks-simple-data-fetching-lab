c4a8d8d82c30b4563350a3e40bf79e9b34f6ca68
Leigh Dotey <leighdotey@gmail.com>
2024-05-28 16:51:05 -0400

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
 // create your App component here
+
+import React, {useState, useEffect} from 'react'
+
+const App = () => {
+    const [dogImage, setDogImage] = useState([])
+    const [loaded, setLoaded] = useState(false)
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then ((resp) => resp.json())
+        .then ((data) => {
+        console.log(data);
+        setDogImage(data.message)
+        setLoaded(true)
+    })
+    }, [])
+    if (!loaded) return <p>Loading...</p>
+    return (
+        <div>
+            <img src={dogImage} alt="A Random Dog"></img>
+
+        </div>
+        
+    )
+}
+
+export default App
